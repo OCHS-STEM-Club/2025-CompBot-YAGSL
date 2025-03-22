bcc923a6f1eea2533270b30b71f3e5f39e6aee0d
princerodrick59 <116687818+princerodrick59@users.noreply.github.com>
2025-03-22 17:16:45 -0500

    swapped back to old path planner code that had working autos

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -188,7 +188,7 @@ public class SwerveSubsystem extends SubsystemBase
           // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
           new PPHolonomicDriveController(
               // PPHolonomicController is the built in path following controller for holonomic drive trains
-              new PIDConstants(5, 0.0, 2.25),
+              new PIDConstants(4, 0.0, 0),
               // Translation PID constants
               new PIDConstants(5, 0.0, 0.0)
               // Rotation PID constants
